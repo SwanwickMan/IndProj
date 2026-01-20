694dcc79737bf3c78eeb17d998416e4bf7d431dc
SwanwickMan <94620554+SwanwickMan@users.noreply.github.com>
2026-01-20 12:11:30 +0000

    Add new files

--- BinarySearch/obj/Debug/net8.0/binarySearch.AssemblyInfo.cs
+++ BinarySearch/obj/Debug/net8.0/binarySearch.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("binarySearch")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+feef9c5667625da7db30d4080c074b44ad65c60e")]
 [assembly: System.Reflection.AssemblyProductAttribute("binarySearch")]
 [assembly: System.Reflection.AssemblyTitleAttribute("binarySearch")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ads2/QuickSort_ADS2/obj/Debug/net8.0/quicksort.AssemblyInfo.cs
+++ ads2/QuickSort_ADS2/obj/Debug/net8.0/quicksort.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("quicksort")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+feef9c5667625da7db30d4080c074b44ad65c60e")]
 [assembly: System.Reflection.AssemblyProductAttribute("quicksort")]
 [assembly: System.Reflection.AssemblyTitleAttribute("quicksort")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
